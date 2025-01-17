24349bb1d5542d4eb2d81c81b04bfff02bed57d6
Sebastiano Calella <sebastianocalella97@gmail.com>
2025-01-17 01:26:32 +0100

    initialize application setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ExpensesListComponent } from './components/expenses-list/expenses-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'expenses', component: ExpensesListComponent },
+  { path: '', redirectTo: '/expenses', pathMatch: 'full' },
+];
